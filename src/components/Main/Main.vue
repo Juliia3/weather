2fb6c39ac8540f8be87401fcac06dbb2973c34db
Yuliia <yulya.sobchenko2001@gmail.com>
2023-07-24 12:19:04 +0300

    API

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -4,6 +4,7 @@ import Card from '../Card/Card.vue'
 import cloud from '@/assets/img/cloudy.svg'
 import bgCloud from '@/assets/img/bg_cloud.png'
 import smallSun from '@/assets/img/small-sun.svg'
+import axios from 'axios'
 
 </script>
 
@@ -18,17 +19,17 @@ import smallSun from '@/assets/img/small-sun.svg'
                 v-model="search"
                 />
                 <ul class="dropdown-content">
-                    <li v-for="item in searchHandler" :key="item.id">
+                    <li v-for="item in searchHandler" :key="item.id" @click="getApi(item.name)">
                         <p>{{ item.name }}</p>
                     </li>
                 </ul>
             </div>
             <Card
-            day="Friday"
+            :day="today"
             :icon="cloud"
-            degrees="21°C"
+            :degrees="currentTemp+' °C'"
             :bg="bgCloud"
-            city="London"
+            :city="currentCity"
             time="13"
             :iconSmall="smallSun"
             degreesAll="25°C"
@@ -43,13 +44,17 @@ import {cities} from '../data/cities'
 export default {
     data() {
         return {
+            currentCity: '',
+            today: '',
+            currentTemp: '',
             search: '',
             data: []
         }
     },
     created() {
         this.data = cities;
-        console.log(this.data);
+        this.getCityByIP();
+        this.today = (new Date).toLocaleDateString("en-US", {weekday: 'long'});
     },
     computed: {
         searchHandler() {
@@ -57,6 +62,28 @@ export default {
                 return this.search.length && elem.name.toLowerCase().includes(this.search.toLowerCase())
             })
         }
+    },
+    methods: {
+        async getApi(city) {
+            const getWeather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city.toLowerCase()}&appid=261514ec6ead072a338a344dce0fb58f`)
+            
+            const data = getWeather.data;
+            this.search = city;
+            this.currentCity = city;
+
+            this.currentTemp = data.main.temp.toFixed(1)
+            
+            this.get7DaysForecast(data.coord.lat, data.coord.lon)
+        },
+        async get7DaysForecast(lat, lon) {
+            const get7Days = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,hourly,minutely,alerts&units=metric&appid=547343e58f273e78496cc775ed43bf31`)
+            console.log(get7Days.data)
+        },
+        async getCityByIP() {
+            const getCity = await axios.get('https://ipapi.co/json/');
+            let data = getCity.data;
+            this.getApi(data.city)
+        },
     }
 }
 
